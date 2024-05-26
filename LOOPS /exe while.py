b74cca42bf9cb642d5d4c44cd28087d477575c5f
dhirendraxd <dhirendraxd@gmail.com>
2024-05-26 06:38:07 +0545

    new changes

---
+++ LOOPS /exe while.py
@@ -0,0 +1,23 @@
+i = 1
+while i <= 100:
+    print(i)
+    i += 1
+
+
+i = 100
+while i >= 1:
+    print(i)
+    i -= 1
+
+
+n =int(input("enter any number for multiplication"))
+i = 1
+while i<=10:
+    print(n*i)
+    i += 1
+
+nums = [1, 2, 3, 4, 5, 6, 7, 7, 8, 7, 8]
+idx = 0
+while idx <len(nums) : 
+    print(nums[idx])
+    idx += 1
---
+++ LOOPS /while.py
@@ -0,0 +1,14 @@
+count = 1
+while count <= 1 :
+    print("hello")
+    count += 1 
+    
+i = 1
+while i<=5 :
+    print("hey")
+    i+=1 # we use this operator to increase the value of that variables to increase iteration 
+i2 = 5
+while i2 >= 1 :
+    print("loop ended")
+    i2 -= 1
+    
