98e4250dfcae619cf1c2e705b953c4c0b0199821
dhirendraxd <dhirendraxd@gmail.com>
2024-05-30 12:15:43 +0545

    new changes

---
+++ FUNCTIONS AND RECURSION/functio.py
@@ -0,0 +1,18 @@
+def function_ha(numb): # function is defined by def 
+
+#    print(numb ** 2) # using print in function and later on will cause errors 
+
+   return numb ** 2 # this will retunr the square of the input num 
+   
+reault = function_ha(4)
+
+print(reault) # this willprint the data stored in reault by value 
+
+print(function_ha(43)) # this will print the ouput of the given 43 
+
+def add(num1,num2):
+    return num1+num2
+
+print(add(34,54))
+
+# polimorphism in function 
