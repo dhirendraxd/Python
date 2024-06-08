329e62cef2c46a670410ac7a99114952e4620c95
dhirendraxd <dhirendraxd@gmail.com>
2024-06-08 09:28:13 +0545

    appending files and creating fils

---
+++ FILE I|O/io(a).py
@@ -0,0 +1,5 @@
+f = open("/home/dhiren/Documents/Codes/Python/hey.txt", "a")
+
+f.write(" \n this line is for appending .......... hey this is usign a in file IO \n ") 
+
+f.close # always should close file we creat 
---
+++ FILE I|O/io(w).py
@@ -0,0 +1,6 @@
+f = open("/home/dhiren/Documents/Codes/Python/hey.txt", "w")
+ # using "w" will delete the any characters in the file and newly write  things 
+
+f.write("dhiren") # will replace this with anything that was in there 
+
+f.close # always should close file we creat 
